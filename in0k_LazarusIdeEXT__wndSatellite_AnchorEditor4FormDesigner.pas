ff3346e7bbfdd6d3bd0a4b4661e31fa545dd40fa
val <val@kreit.ru>
2018-11-09 14:14:18 +0500

    reNames

--- in0k_LazarusIdeEXT__wndSatellite_AnchorEditor4FormDesigner.pas
+++ in0k_LazarusIdeEXT__wndSatellite_AnchorEditor4FormDesigner.pas
@@ -2,7 +2,7 @@
   This source is only used to compile and install the package.
  }
 
-unit in0k_LazIdeEXT_wndSatellite_AnchorEditor_4_FormDesigner;
+unit in0k_LazarusIdeEXT__wndSatellite_AnchorEditor4FormDesigner;
 
 {$warn 5023 off : no warning about unused units}
 interface
@@ -10,7 +10,11 @@ interface
 uses
     in0k_LazarusIdeEXT__REGISTER, in0k_lazarusIdeSRC__expertCORE, 
     in0k_lazarusIdeSRC__wndDEBUG, 
-    in0k_LazarusazIdeEXT__wndSatellite__AnchorEditor_4_FormDesigner, 
+    in0k_lazarusIdePLG__wndSatellite_AnchorEditor4FormDesigner, 
+    in0k_lazarusIdeSRC__tControlWndProc_fuckUp, 
+    in0k_lazarusIdeSRC__fuckUp_onActivate, 
+    in0k_lazarusIdeSRC__ideForm_AnchorEditor, in0k_lazarusIdeSRC__B2SP, 
+    in0k_bringToSecondPlane_LazLCL, in0k_bringToSecondPlane_WinAPI, 
     LazarusPackageIntf;
 
 implementation
@@ -22,6 +26,6 @@ begin
 end;
 
 initialization
-  RegisterPackage('in0k_LazIdeEXT_wndSatellite_AnchorEditor_4_FormDesigner', 
+  RegisterPackage('in0k_LazarusIdeEXT__wndSatellite_AnchorEditor4FormDesigner', 
 			@Register);
 end.
--- in0k_lazExt_DEBUG.pas
+++
@@ -1,4 +0,0 @@
-unit in0k_lazExt_DEBUG;
-{$i in0k_lazIdeSRC_wndDEBUG.inc}
-end.
-
